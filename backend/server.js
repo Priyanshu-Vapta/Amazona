5602ad2d70246c8f8749e9ec3c04b2d36cacb8b7
Priyanshu-Vapta <priyanshuvapta@gmail.com>
2025-04-19 21:41:50 +0530

    Prepare for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -42,7 +43,17 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+// Serve static files
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Catch-all for frontend routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
